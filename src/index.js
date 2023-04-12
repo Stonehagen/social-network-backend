d7afcbe83da8a12520cf72aa11facb5db6a0e138
Stonehagen <steinhagen@mailbox.org>
2023-04-12 22:04:40 +0200

    add database connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
+
+// eslint-disable-next-line operator-linebreak
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'mongo connection error'));
 
 const app = express();
 
@@ -10,5 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // eslint-disable-next-line arrow-body-style
 app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
   return console.log(`app listening on port ${process.env.PORT}!`);
 });
