e377ed9d6034ee10befb5a26fc8dc379f9bfcc8c
Stonehagen <steinhagen@mailbox.org>
2023-04-14 23:49:16 +0200

    add test for delete post

--- tests/postController.test.js
+++ tests/postController.test.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const { startServer, stopServer } = require('./utils/mongoConfigTesting');
 const { fakeUserData, fakeLoginData, fakePostData } = require('./fixtures');
 const { user, post } = require('../src/routes');
+const { Post } = require('../src/models');
 require('../src/config/passport');
 
 const app = express();
@@ -22,14 +23,21 @@ beforeAll(async () => {
   await request(app).post('/user/sign-up').send(fakeUserData);
   const response = await request(app).post('/user/log-in').send(fakeLoginData);
   token = response.body.token;
+
+  // populate the DB with 10 Posts
+  const newPosts = [];
+  for (let i = 0; i < 10; i += 1) {
+    newPosts.push(new Post({ text: `this is post no ${i + 1}` }));
+  }
+  newPosts.forEach((newPost) => newPost.save());
 });
+
 afterAll(async () => stopServer());
 
 describe('/Post/New Route', () => {
   test('new Post Route works', (done) => {
     request(app)
       .post('/post/new')
-      // .auth(token, { type: 'bearer' })
       .send(fakePostData)
       .set('Authorization', `Bearer ${token}`)
       .expect(201, done);
@@ -38,11 +46,38 @@ describe('/Post/New Route', () => {
   test('Invalid post text doesnt works', (done) => {
     request(app)
       .post('/post/new')
-      // .auth(token, { type: 'bearer' })
       .send({
         text: '123',
       })
       .set('Authorization', `Bearer ${token}`)
       .expect(400, done);
+  });
+});
+
+describe('/post/latest/:limit Route', () => {
+  test('latest posts route works', (done) => {
+    request(app)
+      .get('/post/latest/10')
+      .set('Authorization', `Bearer ${token}`)
+      .expect((res) => {
+        expect(res.body.posts.length).toBe(10);
+      })
+      .expect(200, done);
+  });
+});
+
+describe('/post/:postid delete Route', () => {
+  test('delete posts route works', (done) => {
+    request(app)
+      .post('/post/new')
+      .send({ text: 'delete me now' })
+      .set('Authorization', `Bearer ${token}`)
+      .then((res) => {
+        request(app)
+          // eslint-disable-next-line no-underscore-dangle
+          .delete(`/post/${res.body.newPost._id}`)
+          .set('Authorization', `Bearer ${token}`)
+          .expect(204, done);
+      });
   });
 });
