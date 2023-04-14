46a4be90519e00ddbd0418ac2f8c4bc017ec7ba5
Stonehagen <steinhagen@mailbox.org>
2023-04-14 09:03:00 +0200

    removed console log

--- tests/profileModel.test.js
+++ tests/profileModel.test.js
@@ -9,7 +9,6 @@ afterAll(async () => stopServer());
 test('Profile Model works', async () => {
   const newProfile = new Profile(fakeProfileData);
   const savedProfile = await newProfile.save();
-  console.log(savedProfile.firstName);
   expect(savedProfile).not.toBeNull();
   expect(savedProfile.firstName).toBe(fakeProfileData.firstName);
   expect(savedProfile.lastName).toBe(fakeProfileData.lastName);
