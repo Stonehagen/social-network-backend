b979a90f67506127ac6188f2b484d4696826b8ae
Stonehagen <steinhagen@mailbox.org>
2023-04-14 08:44:31 +0200

    add friends and requests

--- src/models/profile.js
+++ src/models/profile.js
@@ -5,6 +5,9 @@ const { Schema } = mongoose;
 const ProfileSchema = new Schema({
   firstName: { type: String, required: true, maxLength: 20 },
   lastName: { type: String, required: true, maxLength: 20 },
+  friends: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
+  friendRequestOut: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
+  friendRequestIn: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
 });
 
 module.exports = mongoose.model('Profile', ProfileSchema);
