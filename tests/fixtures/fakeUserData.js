debcf40fdc473fb369695804178d6f05730a6280
Stonehagen <steinhagen@mailbox.org>
2023-04-15 22:44:54 +0200

    add id to fake user data

--- tests/fixtures/fakeUserData.js
+++ tests/fixtures/fakeUserData.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
+
 exports.fakeUserData = {
+  _id: new mongoose.mongo.ObjectId(),
   firstName: 'Tobias',
   lastName: 'Stonehagen',
   email: 'fake@user.com',
+  password: '12345678',
+};
+
+exports.fakeUserData2 = {
+  _id: new mongoose.mongo.ObjectId(),
+  firstName: 'Horst',
+  lastName: 'Mayer',
+  email: 'hoho@user.com',
   password: '12345678',
 };
--- tests/fixtures/index.js
+++ tests/fixtures/index.js
@@ -1,10 +1,11 @@
-const { fakeUserData } = require('./fakeUserData');
+const { fakeUserData, fakeUserData2 } = require('./fakeUserData');
 const { fakeLoginData } = require('./fakeLoginData');
 const { fakeProfileData } = require('./fakeProfileData');
 const { fakePostData } = require('./fakePostData');
 
 module.exports = {
   fakeUserData,
+  fakeUserData2,
   fakeLoginData,
   fakeProfileData,
   fakePostData,
