25817731ccd09203b1a3c87ea9986454cbe492e3
Stonehagen <steinhagen@mailbox.org>
2023-04-18 22:48:40 +0200

    add session route to get user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const user = require('./user');
 const post = require('./post');
 const profile = require('./profile');
+const session = require('./session');
 
 module.exports = {
   user,
   post,
   profile,
+  session,
 };
