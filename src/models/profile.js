ef5b3425325a38f516fade036356d37f1d604cfd
Stonehagen <steinhagen@mailbox.org>
2023-04-25 16:14:46 +0200

    change default phot in profile model

--- src/models/profile.js
+++ src/models/profile.js
@@ -6,7 +6,7 @@ const ProfileSchema = new Schema({
   firstName: { type: String, required: true, maxLength: 20 },
   lastName: { type: String, required: true, maxLength: 20 },
   status: { type: String, default: 'Cornect with me!', maxLength: 40 },
-  photo: { type: String },
+  photo: { type: String, default: 'profile.jpg' },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   friends: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
   friendRequestOut: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
