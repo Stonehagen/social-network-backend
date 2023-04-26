239a9291a8ea034c42d2cae2d4e98c6dbe7f3e9d
Stonehagen <steinhagen@mailbox.org>
2023-04-26 13:17:59 +0200

    add plublic field to post model

--- src/models/post.js
+++ src/models/post.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 
 const PostSchema = new Schema({
   text: { type: String, required: true, maxLength: 1000 },
+  public: { type: Boolean, default: false },
   timestamp: { type: Date, default: Date.now },
   author: { type: Schema.Types.ObjectId, ref: 'Profile' },
   likes: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
